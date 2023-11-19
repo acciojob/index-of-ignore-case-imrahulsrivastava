f2ff86ddaea50912e056e7f0274803d90ea13baa
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-19 20:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+  if (str === null || subStr === null) {
+    return -1;
+  }
+
+  const lowerStr = str.toLowerCase();
+  const lowerSubStr = subStr.toLowerCase();
+
+  return lowerStr.indexOf(lowerSubStr);
 }
 
 // Please do not change the code below
